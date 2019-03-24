c5746651e981d67dffb0e8b0ff508f974cb861d9
Serge Fabre <serge.fabre@intecbrussel.be>
2019-03-24 17:22:33 +0100

    Add project files.

---
+++ StreamReader/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Streams
+{
+    class Program
+    {
+        static string filePath;
+        static string path;
+
+        static void Main(string[] args)
+        {
+            //createFolder();
+            writeToFile();
+            readFromFile();
+            getAllFiles();
+            Console.WriteLine();
+            getDirectories();
+            
+                Console.ReadKey();
+
+        }
+
+        static string createName()
+        {
+            string fileNameFull = Path.GetRandomFileName();
+            string[] name = fileNameFull.Split('.');
+            return name[0];
+        }
+
+        static string createFolder()
+        {
+            string fileName = createName();
+            fileName += ".txt";
+            
+            string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            path = Path.Combine(folderPath, "test");
+            Directory.CreateDirectory(path);
+
+            filePath = Path.Combine(path, fileName);
+
+            Console.WriteLine(filePath);
+
+            return filePath;
+        }
+
+        static void writeToFile()
+        {
+            using (StreamWriter writer = File.CreateText(createFolder()))
+            {
+                writer.WriteLine("Eerste regel");
+                writer.WriteLine("Tweede regel");
+            }
+        }
+
+        static void readFromFile()
+        {
+            string content;
+            using (StreamReader reader = File.OpenText(filePath))
+            {
+                content = reader.ReadToEnd();
+            }
+
+            Console.WriteLine(content);
+        }
+
+        static void getAllFiles()
+        {
+            string[] files = Directory.GetFiles(path);
+
+            foreach (var item in files)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static void getDirectories()
+        {
+            path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            string[] directories = Directory.GetDirectories(path);
+
+            foreach (var item in directories)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
